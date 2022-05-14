e79754aa3a87c4b5ccd95c9a2788542dbd87d847
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-05-14 16:20:23 +0400

    Add project files

--- src/homework/FigurePainter1.java
+++ src/homework/FigurePainter1.java
@@ -4,10 +4,10 @@ public class FigurePainter1 {
     public static void main(String[] args) {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < i; j++) {
-                System.out.print(" ");
+                System.out.print("  ");
             }
             for (int k = 4; k > i; k--) {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
         }
