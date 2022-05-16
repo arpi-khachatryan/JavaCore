418f478c54e1c5b1558c63584f1aea41f0bc071c
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-05-16 13:46:00 +0400

    Ternary

---
+++ src/chapters/chapter4/Ternary.java
@@ -0,0 +1,14 @@
+package chapters.chapter4;
+
+public class Ternary {
+    public static void main(String[] args) {
+        int i, k;
+        i = 10;
+        k = i < 0 ? -1 : i;
+        System.out.println("i = " + k);
+        i = -10;
+        k = i < 0 ? -i : i;
+        System.out.println("i = " + k);
+    }
+}
+
