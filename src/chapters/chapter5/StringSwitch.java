94df613bf073353fbe0a0cd21118c3e1ecc75f9e
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-05-18 13:29:26 +0400

    StringSwitch

---
+++ src/chapters/chapter5/StringSwitch.java
@@ -0,0 +1,22 @@
+package chapters.chapter5;
+
+public class StringSwitch {
+    public static void main(String[] args) {
+        String str = "one";
+        switch (str) {
+            case "one":
+                System.out.println("one");
+                break;
+            case "two":
+                System.out.println("two");
+                break;
+            case "three":
+                System.out.println("three");
+                break;
+            default:
+                System.out.println("didn't match");
+                break;
+        }
+    }
+}
+
