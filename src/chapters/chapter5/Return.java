e9ccf1234df9feda6b067cf06e1cbd4c863c4da9
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-05-22 17:35:16 +0400

    Return

---
+++ src/chapters/chapter5/Return.java
@@ -0,0 +1,11 @@
+package chapters.chapter5;
+
+public class Return {
+    public static void main(String[] args) {
+        boolean t = true;
+        System.out.println("Մինչեւ վերադարձնելը");
+        if (t) return;
+        System.out.println("Այս օպերատորը չի աշխատի");
+    }
+}
+
