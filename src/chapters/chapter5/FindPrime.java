7a2db2702866807d121b4f7ca28660118f13db40
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-05-22 12:16:28 +0400

    FindPrime

---
+++ src/chapters/chapter5/FindPrime.java
@@ -0,0 +1,25 @@
+package chapters.chapter5;
+
+public class FindPrime {
+    public static void main(String[] args) {
+        int num = 14;
+        boolean isPrime;
+
+        if (num < 2) {
+            isPrime = false;
+        } else {
+            isPrime = true;
+        }
+        for (int i = 2; i <= num; i++) {
+            if ((num % i) == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            System.out.println("Պարզ թիվ");
+        } else {
+            System.out.println("Ոչ պարզ թիվ");
+        }
+    }
+}
