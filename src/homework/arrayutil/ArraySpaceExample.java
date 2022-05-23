ed2d94c54b5be58fe94f370c0c381d737ed7a4b6
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-05-23 21:37:59 +0400

    ArraySpaceExample

--- src/homework/arrayutil/ArraySpaceExample.java
+++ src/homework/arrayutil/ArraySpaceExample.java
@@ -3,12 +3,11 @@ package homework.arrayutil;
 public class ArraySpaceExample {
     public static void main(String[] args) {
         char[] spaceArray = {' ', 'c', 'a', 't', ' ', 'b', 'i', ' ', 'b', ' ', ' '};
-        char[] result = new char[spaceArray.length];
         for (int i = 0; i < spaceArray.length; i++) {
             if (spaceArray[i] != ' ') {
                 for (int j = spaceArray.length - 1; j > i; j--) {
                     if (spaceArray[j] != ' ') {
-                        result = new char[j - i + 1];
+                        char[] result = new char[j - i + 1];
                         for (int k = 0; k < result.length; k++) {
                             result[0] = spaceArray[i++];
                             System.out.print(result[0]);
