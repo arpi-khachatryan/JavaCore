729e170e3d311be2c9a605ef2b75c030f8cf9db4
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-01 14:44:21 +0400

    ArrayUtil2

---
+++ src/homework/arrayutil/ArrayUtil2.java
@@ -0,0 +1,94 @@
+package homework.arrayutil;
+
+public class ArrayUtil2 {
+
+    // first
+    void allArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    // second
+    int max(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    // third
+    int min(int[] array) {
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    // fourth
+    void allEven(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                System.out.print(array[i] + " ");
+            }
+        }
+    }
+
+    // fifth
+    void allOdd(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                System.out.print(array[i] + " ");
+            }
+        }
+    }
+
+    // sixth
+    int even(int[] array) {
+        int even = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                even += 1;
+            }
+        }
+        return even;
+    }
+
+    // seventh
+    int odd(int[] array) {
+        int odd = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                odd += 1;
+            }
+        }
+        return odd;
+    }
+
+    // eighth
+    int average(int[] array) {
+        int average = 0;
+        for (int i = 0; i < array.length; i++) {
+            average += array[i];
+        }
+        return average / array.length;
+    }
+
+    // ninth
+    int sum(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+}
+
+
+
