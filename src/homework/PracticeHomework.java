32eebb158a0e743929cdd6051238007f1c859755
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-01 18:35:17 +0400

    PracticeHomework

--- src/homework/PracticeHomework.java
+++ src/homework/PracticeHomework.java
@@ -5,19 +5,13 @@ public class PracticeHomework {
     //փոխակերպել տրված րոպեն վայրկյանի ու վերադարձնել
     long convert(int minutes) {
         int seconds = 0;
-        if (minutes >= 1) {
-            seconds = minutes * 60;
-        }
-        return seconds;
+        return minutes >= 1 ? seconds = minutes * 60 : seconds;
     }
 
     //ընդունել տարին, վերադարձնել օրերի քանակը
     int calcAge(int years) {
         int days = 0;
-        if (years >= 1) {
-            days = years * 365;
-        }
-        return days;
+        return years >= 1 ? days = years * 365 : days;
     }
 
     //վերադարձնել տրված թվի հաջորդ թիվը
@@ -27,24 +21,14 @@ public class PracticeHomework {
 
     //վերդարձնել true,եթե տրված a եւ b թվերը իրար հավասար են, եթե ոչ՝ false
     boolean isSameNum(int a, int b) {
-        boolean t;
-        if (a == b) {
-            t = true;
-        } else {
-            t = false;
-        }
-        return t;
+        boolean t = false;
+        return a == b ? t = true : t;
     }
 
     //վերդարձնել true,եթե տրված թիվը փոքր կամ հավասար է 0-ի, մնացած դեպքերում՝ false
     boolean lessThanOrEqualToZero(int number) {
-        boolean t;
-        if (number <= 0) {
-            t = true;
-        } else {
-            t = false;
-        }
-        return t;
+        boolean t = false;
+        return number <= 0 ? t = true : t;
     }
 
     //վարադարձնել ընդունող բուլյանի ժխտված արժեքը
@@ -55,11 +39,6 @@ public class PracticeHomework {
     //վերադարձնել երկու մասիվի ամենամեծ length-ը
     int maxLength(int[] array1, int[] array2) {
         int length = 0;
-        if (array1.length > array2.length) {
-            length = array1.length;
-        } else {
-            length = array2.length;
-        }
-        return length;
+        return array1.length > array2.length ? length = array1.length : array2.length;
     }
 }
