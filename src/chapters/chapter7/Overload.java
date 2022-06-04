8638708890edb6d5b34e236e29274e428da2b1a0
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-04 16:32:27 +0400

    Overload

---
+++ src/chapters/chapter7/Overload.java
@@ -0,0 +1,20 @@
+package chapters.chapter7;
+
+public class Overload {
+    void test() {
+        System.out.println("Պարամետրերը բացակայում են");
+    }
+
+    void test(int a) {
+        System.out.println("int a: " + a);
+    }
+
+    void test(int a, int b) {
+        System.out.println("a & b: " + a + " " + b);
+    }
+
+    double test(double a) {
+        System.out.println("double a: " + a);
+        return a * a;
+    }
+}
