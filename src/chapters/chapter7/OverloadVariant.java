45fe683669ee4ecfe61923438f156cabfe142926
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-04 16:51:41 +0400

    OverloadVariant

---
+++ src/chapters/chapter7/OverloadVariant.java
@@ -0,0 +1,16 @@
+package chapters.chapter7;
+
+public class OverloadVariant {
+
+    void test() {
+        System.out.println("Պարամետրերը բացակայում են");
+    }
+
+    void test(int a, int b) {
+        System.out.println("a & b: " + a + " " + b);
+    }
+
+    void test(double a) {
+        System.out.println("double a: " + a);
+    }
+}
