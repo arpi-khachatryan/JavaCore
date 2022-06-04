6a63d423ae3ca309cf9e3360d152b3cc0d4c89b1
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-04 19:04:32 +0400

    OverloadConsDemo

--- src/chapters/chapter7/OverloadConsDemo.java
+++ src/chapters/chapter7/OverloadConsDemo.java
@@ -2,9 +2,9 @@ package chapters.chapter7;
 
 public class OverloadConsDemo {
     public static void main(String[] args) {
-        OverloadCons mybox1 = new OverloadCons(10, 20, 15);
-        OverloadCons mybox2 = new OverloadCons();
-        OverloadCons mycube = new OverloadCons(7);
+        Box mybox1 = new Box(10, 20, 15);
+        Box mybox2 = new Box();
+        Box mycube = new Box(7);
 
         System.out.println(mybox1.volume());
         System.out.println(mybox2.volume());
