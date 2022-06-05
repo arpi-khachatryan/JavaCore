e8bbad4df078fc78bb29f5c1994993bf518bb28e
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-05 22:00:15 +0400

    BraceChecker

--- src/homework/BraceChecker.java
+++ src/homework/BraceChecker.java
@@ -14,7 +14,7 @@ public class BraceChecker {
     public void check(StackClass stack) {
         char[] chars = text.toCharArray();
         for (int i = 0; i < chars.length; i++) {
-            if (chars[i] == '[') {
+            if (chars[i] == '[' | chars[i] == ']' | chars[i] == ')') {
                 stack.push(i);
             }
             if (chars[i] == ']') {
