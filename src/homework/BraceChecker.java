757d1211552ec56b58108e67e6ab88659142408d
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-05 20:45:37 +0400

    BraceChecker

---
+++ src/homework/BraceChecker.java
@@ -0,0 +1,35 @@
+package homework;
+
+
+import chapters.chapter7.StackClass;
+
+public class BraceChecker {
+    private String text;
+
+    public BraceChecker(String text) {
+        this.text = text;
+
+    }
+
+    public void check(StackClass stack) {
+        char[] chars = text.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '[') {
+                stack.push(i);
+            }
+            if (chars[i] == ']') {
+                stack.pop();
+                System.out.println("Opened [ and closed ]");
+            }
+            if (chars[i] == ')') {
+                stack.pop();
+                System.out.println("Error: opened [ but closed ) at " + i);
+            }
+        }
+    }
+}
+
+
+
+
+
