e876478449f92b5c15437e1f6077824c4ae97371
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-05 11:24:18 +0400

    Recursion

---
+++ src/chapters/chapter7/Recursion.java
@@ -0,0 +1,9 @@
+package chapters.chapter7;
+
+public class Recursion {
+    public static void main(String[] args) {
+        Factorial f= new Factorial();
+
+        System.out.println(f.fact(5));
+    }
+}
