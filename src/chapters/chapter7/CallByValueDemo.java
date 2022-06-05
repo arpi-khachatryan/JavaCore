65de7e0305e71db1a00fd7da3a18c7d7df1f876c
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-05 10:14:19 +0400

    CallByValueDemo

---
+++ src/chapters/chapter7/CallByValueDemo.java
@@ -0,0 +1,13 @@
+package chapters.chapter7;
+
+public class CallByValueDemo {
+    public static void main(String[] args) {
+        CallByValue test = new CallByValue();
+        int a = 15;
+        int b = 20;
+        System.out.println("a & b: " + a + " " + b);
+
+        test.meth(a, b);
+        System.out.println("a & b: " + a + " " + b);
+    }
+}
