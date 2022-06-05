253d5ef988f64fde0a6903d848ddd1c2813c5b4c
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-05 23:59:21 +0400

    RecTest

---
+++ src/chapters/chapter7/RecTest.java
@@ -0,0 +1,19 @@
+package chapters.chapter7;
+
+public class RecTest {
+
+    int values[];
+
+    RecTest(int k) {
+        values = new int[k];
+    }
+
+    void printArray(int i) {
+        if (i == 0) {
+            return;
+        } else {
+            printArray((i - 1));
+            System.out.println("[" + (i - 1) + "]" + " " + values[i - 1]);
+        }
+    }
+}
