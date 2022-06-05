bc9d384476a74bf705fc5461edca8f1e2d4fb8a5
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-05 10:40:37 +0400

    PassObjReDemo

---
+++ src/chapters/chapter7/PassObjReDemo.java
@@ -0,0 +1,12 @@
+package chapters.chapter7;
+
+
+public class PassObjReDemo {
+    public static void main(String[] args) {
+        PassObjRe test = new PassObjRe(15, 20);
+        System.out.println("test.a & test.b: " + test.a + " " + test.b);
+
+        test.meth(test);
+        System.out.println("test.a & test.b: " + test.a + " " + test.b);
+    }
+}
