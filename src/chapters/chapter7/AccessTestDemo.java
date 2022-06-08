f6aafd40ca04ccb403a8b13d9ea48acada2cc5e7
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-08 12:06:18 +0400

    AccessTestDemo

---
+++ src/chapters/chapter7/AccessTestDemo.java
@@ -0,0 +1,14 @@
+package chapters.chapter7;
+
+public class AccessTestDemo {
+    public static void main(String[] args) {
+        AccessTest at = new AccessTest();
+
+        at.a = 10;
+        at.b = 20;
+
+        at.setc(30);
+
+        System.out.println("a, b, c: " + at.a + " " + at.b + " " + at.getc());
+    }
+}
