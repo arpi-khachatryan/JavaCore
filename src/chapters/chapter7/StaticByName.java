9b66f79763716ce040218e6ad120b454f00e5cfc
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-08 13:14:23 +0400

    StaticByName

---
+++ src/chapters/chapter7/StaticByName.java
@@ -0,0 +1,10 @@
+package chapters.chapter7;
+
+public class StaticByName {
+    static int a = 42;
+    static int b = 99;
+
+    static void callme() {
+        System.out.println("a = " + a);
+    }
+}
