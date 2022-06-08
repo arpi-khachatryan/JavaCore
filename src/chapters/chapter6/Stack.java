40720e33f12e187c7b11a6faf77179f135f880f8
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-08 12:19:13 +0400

    Stack

--- src/chapters/chapter6/Stack.java
+++ src/chapters/chapter6/Stack.java
@@ -10,7 +10,7 @@ public class Stack {
 
     void push(int item) {
         if (tos == 9) {
-            System.out.println("mStack is full");
+            System.out.println("Stack is full");
         } else {
             stck[++tos] = item;
         }
@@ -18,7 +18,7 @@ public class Stack {
 
     int pop() {
         if (tos < 0) {
-            System.out.println("Stack is full");
+            System.out.println("Stack is not full");
             return 0;
         } else {
             return stck[tos--];
