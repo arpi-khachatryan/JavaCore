558a7827884bd97e96a236ee90de3353ba8065ef
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-08 12:06:53 +0400

    AccessTestDemo

--- src/chapters/chapter7/AccessTestDemo.java
+++ src/chapters/chapter7/AccessTestDemo.java
@@ -3,7 +3,6 @@ package chapters.chapter7;
 public class AccessTestDemo {
     public static void main(String[] args) {
         AccessTest at = new AccessTest();
-
         at.a = 10;
         at.b = 20;
 
