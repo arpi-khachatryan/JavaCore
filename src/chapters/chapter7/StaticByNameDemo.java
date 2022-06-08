85f2d81ed0fbf2dc329574bf94ce45f776ff9fbc
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-08 13:14:36 +0400

    StaticByNameDemo

---
+++ src/chapters/chapter7/StaticByNameDemo.java
@@ -0,0 +1,8 @@
+package chapters.chapter7;
+
+public class StaticByNameDemo {
+    public static void main(String[] args) {
+        StaticByName.callme();
+        System.out.println("b = " + StaticByName.b);
+    }
+}
