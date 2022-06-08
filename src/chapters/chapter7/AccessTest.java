715c8198b4ed5af5eee0a7a4c47731f26a4c7c6b
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-08 12:06:01 +0400

    AccessTest

---
+++ src/chapters/chapter7/AccessTest.java
@@ -0,0 +1,16 @@
+package chapters.chapter7;
+
+public class AccessTest {
+
+    int a;
+    public int b;
+    private int c;
+
+    void setc(int i) {
+        c = i;
+    }
+
+    int getc() {
+        return c;
+    }
+}
