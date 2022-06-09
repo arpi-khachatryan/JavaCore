9d8c6a5d9a9862bb0fe6fe8fdec37f26f0c80d62
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-09 18:28:54 +0400

    DynamicArray

---
+++ src/homework/arrayutil/DynamicArray.java
@@ -0,0 +1,27 @@
+package homework.arrayutil;
+
+public class DynamicArray {
+    int[] array = new int[10];
+    int size = 0;
+
+    void add(int value) {
+        if (size == array.length) {
+            increaseArray();
+        }
+        array[size++] = value;
+    }
+
+    void increaseArray() {
+        int[] tempArray = new int[array.length + 10];
+        for (int i = 0; i < size; i++) {
+            tempArray[i] = array[i];
+        }
+        array = tempArray;
+    }
+
+    void print() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
