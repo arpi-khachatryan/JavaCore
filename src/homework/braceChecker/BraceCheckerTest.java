c040312bf527dda1e28b8833d4ea28cdd4b92969
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-10 09:31:51 +0400

    BraceCheckerTest

--- src/homework/braceChecker/BraceCheckerTest.java
+++ src/homework/braceChecker/BraceCheckerTest.java
@@ -2,8 +2,7 @@ package homework.braceChecker;
 
 public class BraceCheckerTest {
     public static void main(String[] args) {
-        String text = "[Hello)[]{from] (Java]";
-        BraceChecker ob = new BraceChecker(text);
+        BraceChecker ob = new BraceChecker(")[Hello)[]{from]} (Java])]");
         StackClass mystack = new StackClass();
         ob.check(mystack);
     }
