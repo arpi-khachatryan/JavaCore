1100182392e9a40498fc5b9b7ece75097b6dc04e
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-12 17:38:53 +0400

    DynamicArray

--- src/homework/dynamicArray/DynamicArray.java
+++ src/homework/dynamicArray/DynamicArray.java
@@ -11,7 +11,7 @@ public class DynamicArray {
         array[size++] = value;
     }
 
-    public void increaseArray() {
+    private void increaseArray() {
         int[] tempArray = new int[array.length + 10];
         for (int i = 0; i < size; i++) {
             tempArray[i] = array[i];
@@ -21,29 +21,29 @@ public class DynamicArray {
 
     public void print() {
         for (int i = 0; i < size; i++) {
-            System.out.println(array[i]);
+            System.out.print(array[i] + " ");
         }
+        System.out.println();
     }
 
     public boolean isEmpaty() {
-        if (size == 0) {
-            return true;
-        }
-        return false;
+        return size == 0;
     }
 
     public int getByIndex(int index) {
         for (int i = 0; i < array.length; i++) {
-            if (index == i)
+            if (index == i) {
                 return array[i];
+            }
         }
         return 0;
     }
 
     public int getFirstIndexByValue(int value) {
         for (int i = 0; i < array.length; i++) {
-            if (array[i] == value)
+            if (array[i] == value) {
                 return i;
+            }
         }
         return 0;
     }
@@ -54,13 +54,13 @@ public class DynamicArray {
     }
 
     public void add(int index, int value) {
-        int[] arr = new int[array.length + 1];
+        int[] arr = new int[array.length + 10];
         for (int i = 0; i < array.length; i++) {
             arr[i] = array[i];
             if (i == index) {
                 arr[i] = value;
                 ++i;
-                for (int j = i; j < arr.length; j++) {
+                for (int j = i; j < array.length; j++) {
                     arr[j] = array[j - 1];
                 }
                 break;
@@ -68,13 +68,15 @@ public class DynamicArray {
         }
         array = arr;
         for (int x : array) {
-            System.out.println(x);
+            System.out.print(x + " ");
         }
     }
 
     public void delete(int index) {
         for (int i = 0; i < size; i++) {
-            array[index] = 0;
+            if (array[i] == array[index]) {
+                ++i;
+            }
             System.out.print(array[i] + " ");
         }
     }
