c01ba19b8b86af1c900914c83cd7b34084367f1b
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-13 00:23:41 +0400

    BraceCheckerDemo2

---
+++ src/homework/braceChecker/BraceCheckerDemo2.java
@@ -0,0 +1,8 @@
+package homework.braceChecker;
+
+public class BraceCheckerDemo2 {
+    public static void main(String[] args) {
+        BraceChecker2 bc = new BraceChecker2("){Hello from ([{Java])}");
+        bc.chack();
+    }
+}
