f1a6f2336f1d1c6fcd9d89df3b19b2a494d20b45
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-14 12:51:06 +0400

    VarArgsDemo3

---
+++ src/chapters/chapter7/varargs/VarArgsDemo3.java
@@ -0,0 +1,9 @@
+package chapters.chapter7.varargs;
+
+public class VarArgsDemo3 {
+    public static void main(String[] args) {
+        VarArgs3.vaTest(1, 2, 3);
+        VarArgs3.vaTest(false, true, true);
+        VarArgs3.vaTest("Փորձարկում: ", 10, 20);
+    }
+}
