f9234627f141e21d08a174264571f27adcc811a0
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-14 12:09:27 +0400

    VarArgsDemo

---
+++ src/chapters/chapter7/varargs/VarArgsDemo.java
@@ -0,0 +1,9 @@
+package chapters.chapter7.varargs;
+
+public class VarArgsDemo {
+    public static void main(String[] args) {
+        VarArgs.vaTest(10);
+        VarArgs.vaTest(1, 2, 3);
+        VarArgs.vaTest();
+    }
+}
