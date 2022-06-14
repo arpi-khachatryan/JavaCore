04eced054aab0ba1521fe5d48833d9ced2742c37
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-14 12:35:25 +0400

    VarArgsDemo2

---
+++ src/chapters/chapter7/varargs/VarArgsDemo2.java
@@ -0,0 +1,9 @@
+package chapters.chapter7.varargs;
+
+public class VarArgsDemo2 {
+    public static void main(String[] args) {
+        VarArgs2.vaTest("Մեկ պարամետր փոփոխական երկարությամբ ", 10);
+        VarArgs2.vaTest("Երեք պարամետր փափոխական երկարությամբ ", 1, 2, 3);
+        VarArgs2.vaTest("Պարամետր փափոխական երկարությամբ ");
+    }
+}
