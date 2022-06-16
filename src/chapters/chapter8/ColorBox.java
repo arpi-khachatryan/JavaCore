4b794e4570a99910a7d5567394d472a70ba67c16
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-16 14:20:02 +0400

    ColorBox

---
+++ src/chapters/chapter8/ColorBox.java
@@ -0,0 +1,12 @@
+package chapters.chapter8;
+
+public class ColorBox extends Box {
+    String color;
+
+    ColorBox(double w, double h, double d, String c) {
+        width = w;
+        height = h;
+        depth = d;
+        color = c;
+    }
+}
