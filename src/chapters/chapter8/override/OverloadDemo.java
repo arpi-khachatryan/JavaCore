449531f501eb22abbe5c5a0989b8250021ab7157
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-17 11:39:58 +0400

    OverloadDemo

--- src/chapters/chapter8/override/OverloadDemo.java
+++ src/chapters/chapter8/override/OverloadDemo.java
@@ -1,8 +1,8 @@
 package chapters.chapter8.override;
 
-public class OverrideDemo {
+public class OverloadDemo {
     public static void main(String[] args) {
-        OverrideB b = new OverrideB(1, 2, 3);
+        OverloadB b = new OverloadB(1, 2, 3);
 
         b.show("This is k: ");
         b.show();
