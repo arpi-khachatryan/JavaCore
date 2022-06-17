6b81143b452606b89ff6d1b5af99057fc10cce4a
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-17 11:02:33 +0400

    A

---
+++ src/chapters/chapter8/override/A.java
@@ -0,0 +1,14 @@
+package chapters.chapter8.override;
+
+public class A {
+    int i, j;
+
+    A(int a, int b) {
+        i = a;
+        j = b;
+    }
+
+    void show() {
+        System.out.println(i + " " + j);
+    }
+}
