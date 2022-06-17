da674edd8181decfc4c5e35c9247bf0b784c5a3f
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-17 11:11:06 +0400

    OverrideB

---
+++ src/chapters/chapter8/override/OverrideDemo.java
@@ -0,0 +1,10 @@
+package chapters.chapter8.override;
+
+public class OverrideDemo {
+    public static void main(String[] args) {
+        OverrideB b = new OverrideB(1, 2, 3);
+
+        b.show("This is k: ");
+        b.show();
+    }
+}
