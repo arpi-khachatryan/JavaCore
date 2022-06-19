c86172be3124fb05cd2032e7181cd7e920538447
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-19 10:36:47 +0400

    AbstractDemo

---
+++ src/chapters/chapter8/abstractClasses/AbstractDemo.java
@@ -0,0 +1,9 @@
+package chapters.chapter8.abstractClasses;
+
+public class AbstractDemo {
+    public static void main(String[] args) {
+        B b = new B();
+        b.callme();
+        b.callmetoo();
+    }
+}
