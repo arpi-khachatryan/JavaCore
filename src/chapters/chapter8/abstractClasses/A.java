d09c656758336e25522e730e0baa97e646c2459c
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-19 10:36:09 +0400

    A

---
+++ src/chapters/chapter8/abstractClasses/A.java
@@ -0,0 +1,9 @@
+package chapters.chapter8.abstractClasses;
+
+abstract public class A {
+    abstract void callme();
+
+    void callmetoo() {
+        System.out.println("Սա կոնկրետ մեթոդ է");
+    }
+}
