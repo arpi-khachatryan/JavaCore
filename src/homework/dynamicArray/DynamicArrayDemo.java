c0efe3739fe627d76807b81d654ff155df82531b
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-20 10:36:17 +0400

    DynamicArrayDemo

--- src/homework/dynamicArray/DynamicArrayDemo.java
+++ src/homework/dynamicArray/DynamicArrayDemo.java
@@ -3,20 +3,20 @@ package homework.dynamicArray;
 public class DynamicArrayDemo {
     public static void main(String[] args) {
         DynamicArray da = new DynamicArray();
-        for (int i = 0; i < 7; i++) {
+        for (int i = 0; i < 6; i++) {
             da.add(i);
         }
         System.out.println(da.isEmpaty());
 
-        System.out.println(da.getByIndex(2));
+        System.out.println(da.getByIndex(-1));
 
         System.out.println(da.getFirstIndexByValue(2));
 
         da.setArray(1, 18);
 
-        da.add(5, 10);
+        da.add(2, 10);
 
-        da.delete(8);
+        da.delete(2);
 
         da.print();
     }
