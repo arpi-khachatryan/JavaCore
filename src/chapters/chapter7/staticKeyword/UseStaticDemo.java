00e6a80d97f1d57a6dcbec097204492f6566185c
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-21 12:43:06 +0400

    UseStaticDemo

--- src/chapters/chapter7/staticKeyword/UseStaticDemo.java
+++ src/chapters/chapter7/staticKeyword/UseStaticDemo.java
@@ -1,4 +1,6 @@
-package chapters.chapter7;
+package chapters.chapter7.staticKeyword;
+
+import chapters.chapter7.staticKeyword.UseStatic;
 
 public class UseStaticDemo {
     public static void main(String[] args) {
