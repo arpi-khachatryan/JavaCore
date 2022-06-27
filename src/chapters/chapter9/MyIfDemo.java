ebb0f4e45af091df2cc8b9a88938b7040035331c
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-27 13:45:45 +0400

    MyIfDemo

---
+++ src/chapters/chapter9/MyIfDemo.java
@@ -0,0 +1,8 @@
+package chapters.chapter9;
+
+public class MyIfDemo {
+    public static void main(String[] args) {
+        int defnum = MyIf.getDefaultNumber();
+        System.out.println(defnum);
+    }
+}
