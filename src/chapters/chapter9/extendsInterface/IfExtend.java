5760d9614529f111336ef68d52953bdd1c6d8f51
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-27 11:15:28 +0400

    IfExtend

---
+++ src/chapters/chapter9/extendsInterface/IfExtend.java
@@ -0,0 +1,10 @@
+package chapters.chapter9.extendsInterface;
+
+public class IfExtend {
+    public static void main(String[] args) {
+        MyClass myClass = new MyClass();
+        myClass.meth1();
+        myClass.meth2();
+        myClass.meth3();
+    }
+}
