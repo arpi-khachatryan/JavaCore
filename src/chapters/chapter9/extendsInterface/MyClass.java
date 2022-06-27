caf541ac1bcd07ce3c1f7318da09870b7a85b9ca
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-27 11:16:01 +0400

    MyClass

---
+++ src/chapters/chapter9/extendsInterface/MyClass.java
@@ -0,0 +1,15 @@
+package chapters.chapter9.extendsInterface;
+
+public class MyClass implements B {
+    public void meth1() {
+        System.out.println("meth1();");
+    }
+
+    public void meth2() {
+        System.out.println("meth2();");
+    }
+
+    public void meth3() {
+        System.out.println("meth3();");
+    }
+}
