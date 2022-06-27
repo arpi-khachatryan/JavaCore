3dd4362ae9f475efd96eb684b896ca0412aebd2a
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-27 12:23:06 +0400

    MyIfImp2

---
+++ src/chapters/chapter9/defaultMethod/MyIfImp2.java
@@ -0,0 +1,11 @@
+package chapters.chapter9.defaultMethod;
+
+public class MyIfImp2 implements MyIf {
+    public int getNumber() {
+        return 2;
+    }
+
+    public String getString() {
+        return "getStringFromMyIfLmp2";
+    }
+}
