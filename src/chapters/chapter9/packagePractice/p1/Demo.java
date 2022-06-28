466d162879ecbca15bbef90a3d3d20386cd70fa1
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-28 13:34:21 +0400

    Demo

---
+++ src/chapters/chapter9/packagePractice/p1/Demo.java
@@ -0,0 +1,9 @@
+package chapters.chapter9.packagePractice.p1;
+
+public class Demo {
+    public static void main(String[] args) {
+        Protection ob1 = new Protection();
+        Derived ob2 = new Derived();
+        SamePackage ob3 = new SamePackage();
+    }
+}
