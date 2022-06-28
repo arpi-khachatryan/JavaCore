38372a2e1ae0cbdecf2cad1cb1048611b0ff969a
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-06-28 13:33:13 +0400

    Protection2

---
+++ src/chapters/chapter9/packagePractice/p2/Protection2.java
@@ -0,0 +1,11 @@
+package chapters.chapter9.packagePractice.p2;
+
+import chapters.chapter9.packagePractice.p1.Protection;
+
+public class Protection2 extends Protection {
+    Protection2() {
+        System.out.println("Protection2");
+        System.out.println("n_pro = " + n_pro);
+        System.out.println("n_pub = " + n_pub);
+    }
+}
