2764ae96c8baefce67bc25fd1bf29e92fa19c794
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-07-07 17:39:21 +0400

    AutoBox5

---
+++ src/chapters/chapter12/wrapperClass/AutoBox5.java
@@ -0,0 +1,14 @@
+package chapters.chapter12.wrapperClass;
+
+public class AutoBox5 {
+    public static void main(String[] args) {
+        Boolean b = true;
+        if (b) {
+            System.out.println("b = true");
+        }
+
+        Character ch = 'x';
+        char ch2 = ch;
+        System.out.println("ch2 = " + ch2);
+    }
+}
