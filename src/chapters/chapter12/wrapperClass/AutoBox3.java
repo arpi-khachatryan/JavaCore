e7428a4062bef06201599187da9b9c8f7b68062f
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-07-07 17:38:59 +0400

    AutoBox3

---
+++ src/chapters/chapter12/wrapperClass/AutoBox3.java
@@ -0,0 +1,20 @@
+package chapters.chapter12.wrapperClass;
+
+public class AutoBox3 {
+    public static void main(String[] args) {
+        Integer iOb, iOb2;
+        int i;
+
+        iOb = 100;
+        System.out.println("iOb: " + iOb);
+
+        ++iOb;
+        System.out.println("++iOb: " + iOb);
+
+        iOb2 = iOb + (iOb / 3);
+        System.out.println("iOb2: " + iOb2);
+
+        i = iOb + (iOb / 3);
+        System.out.println("i: " + i);
+    }
+}
