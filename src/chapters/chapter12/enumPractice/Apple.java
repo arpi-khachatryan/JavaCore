768ad7e48f8d0de1dbd49a5c6d1d623cb766d801
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-07-07 13:47:10 +0400

    Apple

--- src/chapters/chapter12/enumPractice/Apple.java
+++ src/chapters/chapter12/enumPractice/Apple.java
@@ -1,9 +1,27 @@
 package chapters.chapter12.enumPractice;
 
 public enum Apple {
-    JONATHAN,
-    GOLDEN_DEL,
-    RED_DEL,
-    WINESAP,
-    CORTLAND
+    JONATHAN(250),
+    GOLDEN_DEL(150),
+    RED_DEL(300),
+    WINESAP(400),
+    CORTLAND();
+
+    private int price;
+
+    Apple(int price) {
+        this.price = price;
+    }
+
+    Apple() {
+        price = -1;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
