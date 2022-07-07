b58be477c58372d2331e743ec1a2ec3a31790839
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-07-07 16:52:55 +0400

    AutoBox

---
+++ src/chapters/chapter12/wrapperClass/AutoBox.java
@@ -0,0 +1,11 @@
+package chapters.chapter12.wrapperClass;
+
+public class AutoBox {
+    public static void main(String[] args) {
+        Integer iOb = 100;
+
+        int i = iOb;
+
+        System.out.println(i + " " + iOb);
+    }
+}
