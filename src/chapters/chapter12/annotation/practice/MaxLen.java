f9c949e80324fbef9aa4f3c75b487fb5397f364c
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-07-11 17:35:50 +0400

    MaxLen

---
+++ src/chapters/chapter12/annotation/practice/MaxLen.java
@@ -0,0 +1,9 @@
+package chapters.chapter12.annotation.practice;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE_USE)
+public @interface MaxLen {
+    int value();
+}
