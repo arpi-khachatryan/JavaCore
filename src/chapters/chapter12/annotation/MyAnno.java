a34fa1c2c7f0d3d662124a7c2e9e50d53709fd7a
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-07-11 12:05:47 +0400

    MyAnno

---
+++ src/chapters/chapter12/annotation/MyAnno.java
@@ -0,0 +1,11 @@
+package chapters.chapter12.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MyAnno {
+    String str();
+
+    int val();
+}
