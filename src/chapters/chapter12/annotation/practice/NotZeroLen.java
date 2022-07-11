700402fedc76abb989e495f69ea2372dc9acf30b
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-07-11 17:36:24 +0400

    NotZeroLen

---
+++ src/chapters/chapter12/annotation/practice/NotZeroLen.java
@@ -0,0 +1,8 @@
+package chapters.chapter12.annotation.practice;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE_USE)
+public @interface NotZeroLen {
+}
