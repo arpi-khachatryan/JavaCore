8b887b38c627a519ccf39b052100117bd31c238c
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-07-12 01:21:06 +0400

    User

---
+++ src/homework/books/model/User.java
@@ -0,0 +1,70 @@
+package homework.books.model;
+
+public class User {
+    private String name;
+    private String surName;
+    private String email;
+    private String password;
+    private Role registeredRole;
+
+    public User(String name, String surName, String email, String password, Role registeredRole) {
+        this.name = name;
+        this.surName = surName;
+        this.email = email;
+        this.password = password;
+        this.registeredRole = registeredRole;
+    }
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRegisteredRole() {
+        return registeredRole;
+    }
+
+    public void setRegisteredRole(Role registeredRole) {
+        this.registeredRole = registeredRole;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", surName='" + surName + '\'' +
+                ", email='" + email + '\'' +
+                ", registeredRole=" + registeredRole +
+                '}';
+    }
+}
