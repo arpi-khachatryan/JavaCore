ec6987b8185b7a62ce7507cbc574378085dbd275
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-07-11 17:34:32 +0400

    What

---
+++ src/chapters/chapter12/annotation/practice/What.java
@@ -0,0 +1,10 @@
+package chapters.chapter12.annotation.practice;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE_USE)
+public @interface What {
+    String description();
+}
