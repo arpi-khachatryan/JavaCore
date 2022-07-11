de8eade1f709fb2a53e61438caf37f69e3f9cabd
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-07-11 14:12:12 +0400

    MyAnnoDefault

--- src/chapters/chapter12/annotation/MyAnnoDefault.java
+++ src/chapters/chapter12/annotation/MyAnnoDefault.java
@@ -1,10 +1,10 @@
-package chapters.chapter12.annotation.practice;
+package chapters.chapter12.annotation;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
-public @interface MyAnno {
+public @interface MyAnnoDefault {
     String str() default "Թեստավորում";
 
     int i() default 200;
