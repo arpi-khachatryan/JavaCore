d3d51d0d511e5f14bc1ce8b85d6408dc08c401e8
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-07-12 15:10:39 +0400

    UserCommands

---
+++ src/classwork/students/command/UserCommands.java
@@ -0,0 +1,17 @@
+package classwork.students.command;
+
+public interface UserCommands extends Commands {
+    int PRINT_STUDENT_BY_LESSON = 4;
+    int PRINT_LESSONS = 5;
+
+    static void printUserCommands() {
+        System.out.println("Please input " + LOGOUT + " for logout");
+        System.out.println("Please input " + ADD_STUDENT + " to add student");
+        System.out.println("Please input " + PRINT_ALL_STUDENTS + " to print the student/students");
+        System.out.println("Please input " + PRINT_STUDENTS_COUNT + " to print the number of students");
+        System.out.println("Please input " + PRINT_STUDENT_BY_LESSON + " to print the student/students by lesson");
+        System.out.println("Please input " + PRINT_LESSONS + " to print all lessons");
+    }
+}
+
+
