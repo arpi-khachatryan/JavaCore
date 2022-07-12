09b51e4b4f28a939f97f1fb3c84d6b48f7d5a958
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-07-12 11:09:43 +0400

    Book

--- src/homework/books/model/Book.java
+++ src/homework/books/model/Book.java
@@ -1,21 +1,25 @@
 package homework.books.model;
 
+import classwork.students.model.User;
+
 public class Book {
     private String title;
     private Author author;
     private double count;
     private String genre;
     private double price;
+    private User registeredUser;
 
-    public Book(String title, Author author, double count, String genre, double price) {
+    public Book(String title, Author author, double count, String genre, double price, User registeredUser) {
         this.title = title;
         this.author = author;
         this.count = count;
         this.genre = genre;
         this.price = price;
+        this.registeredUser = registeredUser;
     }
 
-    public Book() {
+    public Book(String title, Author author, double count, String genre, double price, homework.books.model.User currentUser) {
     }
 
     public String getTitle() {
@@ -58,6 +62,14 @@ public class Book {
         this.price = price;
     }
 
+    public User getRegisteredUser() {
+        return registeredUser;
+    }
+
+    public void setRegisteredUser(User registeredUser) {
+        this.registeredUser = registeredUser;
+    }
+
     @Override
     public String toString() {
         return "Book{" +
@@ -66,6 +78,7 @@ public class Book {
                 ", count=" + count +
                 ", genre='" + genre + '\'' +
                 ", price=" + price +
+                ", registeredUser=" + registeredUser +
                 '}';
     }
 }
