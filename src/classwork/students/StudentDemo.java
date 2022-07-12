6d1fc9ab667119c4a55a2e098d6c859fa939e4d1
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-07-12 15:11:55 +0400

    StudentDemo

--- src/classwork/students/StudentDemo.java
+++ src/classwork/students/StudentDemo.java
@@ -1,25 +1,112 @@
 package classwork.students;
 
 import classwork.students.command.Commands;
+import classwork.students.command.UserCommands;
 import classwork.students.model.Lesson;
+import classwork.students.model.Role;
 import classwork.students.model.Student;
+import classwork.students.model.User;
 import classwork.students.storage.LessonStorage;
 import classwork.students.storage.StudentStorage;
+import classwork.students.storage.UserStorage;
 
 import java.util.Scanner;
 
-public class StudentDemo implements Commands {
+public class StudentDemo implements Commands, UserCommands {
     private static Scanner scanner = new Scanner(System.in);
     private static StudentStorage studentStorage = new StudentStorage();
     private static LessonStorage lessonStorage = new LessonStorage();
+    private static UserStorage userStorage = new UserStorage();
+    private static User currentUser = null;
 
     public static void main(String[] args) {
+        Lesson java = new Lesson("java", "Galstyan", 5, 5000);
+        lessonStorage.add(java);
+        User admin = new User("admin", "admin", "admin@gmail.com", "admin", Role.ADMIN);
+        userStorage.add(admin);
+        studentStorage.add(new Student("Tigran", "Tigranyan", 18, "099", "Yerevan", java, admin));
         boolean run = true;
         while (run) {
-            Commands.printCommands();
-            int command = Integer.parseInt(scanner.nextLine());
+            Commands.printLoginCommands();
+            int command;
+            try {
+                command = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                command = -1;
+            }
             switch (command) {
                 case EXIT:
+                    run = false;
+                    break;
+                case LOGIN:
+                    login();
+                    break;
+                case REGISTER:
+                    register();
+                    break;
+                default:
+                    System.out.println("Invalid command, please try again");
+            }
+        }
+    }
+
+    private static void login() {
+        System.out.println("Please input email,password");
+        String emilPasswordStr = scanner.nextLine();
+        String[] emailPassword = emilPasswordStr.split(",");
+        User user = userStorage.getUserByEmailAndPassword(emailPassword[0]);
+        if (user == null) {
+            System.out.println("User does not exist");
+        } else {
+            if (!user.getPassword().equals(emailPassword[1])) {
+                System.out.println("Password is incorrect");
+            } else {
+                currentUser = user;
+                if (user.getRole() == Role.ADMIN) {
+                    adminLogin();
+                } else if (user.getRole() == Role.USER) {
+                    userLogin();
+                }
+            }
+        }
+    }
+
+    private static void register() {
+        System.out.println("Please input name,surname,email,password");
+        String userDataStr = scanner.nextLine();
+        String[] userData = userDataStr.split(",");
+        if (userData.length < 4) {
+            System.out.println("Please input correct user data");
+        } else {
+            if (userStorage.getUserByEmailAndPassword(userData[2]) == null) {
+                User user = new User();
+                user.setName(userData[0]);
+                user.setSurName(userData[1]);
+                user.setEmail(userData[2]);
+                user.setPassword(userData[3]);
+                user.setRole(Role.USER);
+                userStorage.add(user);
+                System.out.println("You are registered");
+            } else {
+                System.out.println("User with " + userData[2] + " already exists");
+            }
+        }
+    }
+
+    private static void adminLogin() {
+        System.out.println("Welcome " + currentUser.getName());
+        boolean run = true;
+        while (run) {
+            Commands.printAdminCommands();
+            int command;
+            try {
+                command = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                command = -1;
+            }
+            switch (command) {
+                case LOGOUT:
+                    currentUser = null;
                     run = false;
                     break;
                 case ADD_STUDENT:
@@ -52,6 +139,43 @@ public class StudentDemo implements Commands {
         }
     }
 
+    private static void userLogin() {
+        System.out.println("Welcome " + currentUser.getName());
+        boolean run = true;
+        while (run) {
+            UserCommands.printUserCommands();
+            int command;
+            try {
+                command = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                command = -1;
+            }
+            switch (command) {
+                case LOGOUT:
+                    currentUser = null;
+                    run = false;
+                    break;
+                case ADD_STUDENT:
+                    addStudent();
+                    break;
+                case PRINT_ALL_STUDENTS:
+                    studentStorage.print();
+                    break;
+                case PRINT_STUDENTS_COUNT:
+                    System.out.println(studentStorage.getSize());
+                    break;
+                case PRINT_STUDENT_BY_LESSON:
+                    printStudentByLesson();
+                    break;
+                case PRINT_LESSONS:
+                    lessonStorage.print();
+                    break;
+                default:
+                    System.out.println("Invalid command, please try again");
+            }
+        }
+    }
+
     private static void addStudent() {
         if (lessonStorage.getSize() == 0) {
             System.out.println("Please add the lesson");
@@ -59,39 +183,49 @@ public class StudentDemo implements Commands {
         } else {
             lessonStorage.print();
             System.out.println("Please choose the index of the lesson");
-            int lessonIndex = Integer.parseInt(scanner.nextLine());
-            Lesson lesson = lessonStorage.getLessonByIndex(lessonIndex);
-            if (lesson == null) {
-                System.out.println("Please input correct index");
-                //addStudent();
-            } else {
-                System.out.println("Please input the student's name");
-                String name = scanner.nextLine();
+            try {
+                int lessonIndex = Integer.parseInt(scanner.nextLine());
+                Lesson lesson = lessonStorage.getLessonByIndex(lessonIndex);
+                if (lesson == null) {
+                    System.out.println("Please input correct index");
+                    addStudent();
+                } else {
+                    System.out.println("Please input the student's name");
+                    String name = scanner.nextLine();
 
-                System.out.println("Please input the student's surname");
-                String surname = scanner.nextLine();
+                    System.out.println("Please input the student's surname");
+                    String surname = scanner.nextLine();
 
-                System.out.println("Please input the student's age");
-                int age = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Please input the student's age");
+                    int age = Integer.parseInt(scanner.nextLine());
 
-                System.out.println("Please input the student's phoneNumber");
-                String phoneNumber = scanner.nextLine();
+                    System.out.println("Please input the student's phoneNumber");
+                    String phoneNumber = scanner.nextLine();
 
-                System.out.println("Please input the student's city");
-                String city = scanner.nextLine();
+                    System.out.println("Please input the student's city");
+                    String city = scanner.nextLine();
 
-                Student student = new Student(name, surname, age, phoneNumber, city, lesson);
-                studentStorage.add(student);
-                System.out.println("Thank you, student was added");
+                    Student student = new Student(name, surname, age, phoneNumber, city, lesson, currentUser);
+                    studentStorage.add(student);
+                    System.out.println("Thank you, student was added");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Please input only a number");
+                addStudent();
             }
         }
     }
 
     private static void deleteStudentByIndex() {
-        studentStorage.print();
-        System.out.println("Please choose student's index");
-        int index = Integer.parseInt(scanner.nextLine());
-        studentStorage.delete(index);
+        try {
+            studentStorage.print();
+            System.out.println("Please choose student's index");
+            int index = Integer.parseInt(scanner.nextLine());
+            studentStorage.delete(index);
+        } catch (NumberFormatException e) {
+            System.out.println("Please input only a number");
+            deleteStudentByIndex();
+        }
     }
 
     private static void printStudentByLesson() {
@@ -103,22 +237,32 @@ public class StudentDemo implements Commands {
     private static void changeLesson() {
         studentStorage.print();
         System.out.println("Please select the index");
-        int newIndex = Integer.parseInt(scanner.nextLine());
-        Student student = studentStorage.getStudentByIndex(newIndex);
-        if (student != null) {
-            lessonStorage.print();
-            System.out.println("Please choose lesson index");
-            int lessonIndex = Integer.parseInt(scanner.nextLine());
-            Lesson lesson = lessonStorage.getLessonByIndex(lessonIndex);
-            if (lesson == null) {
-                System.out.println("Please input correct index");
-                changeLesson();
+        try {
+            int newIndex = Integer.parseInt(scanner.nextLine());
+            Student student = studentStorage.getStudentByIndex(newIndex);
+            if (student != null) {
+                lessonStorage.print();
+                System.out.println("Please choose lesson index");
+                try {
+                    int lessonIndex = Integer.parseInt(scanner.nextLine());
+                    Lesson lesson = lessonStorage.getLessonByIndex(lessonIndex);
+                    if (lesson != null) {
+                        student.setLesson(lesson);
+                        System.out.println("Lesson changed");
+                    } else {
+                        System.out.println("Please input correct index");
+                        changeLesson();
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Please input only a number");
+                    changeLesson();
+                }
             } else {
-                student.setLesson(lesson);
-                System.out.println("Lesson changed");
+                System.out.println("Invalid index, please try again");
+                changeLesson();
             }
-        } else {
-            System.out.println("Invalid index, please try again");
+        } catch (NumberFormatException e) {
+            System.out.println("Please input only a number");
             changeLesson();
         }
     }
