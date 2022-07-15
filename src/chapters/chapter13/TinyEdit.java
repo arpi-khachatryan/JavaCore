878be33aa03b522385d9bc4e2ef983559e35ba7d
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-07-15 23:43:53 +0400

    TinyEdit

---
+++ src/chapters/chapter13/TinyEdit.java
@@ -0,0 +1,21 @@
+package chapters.chapter13;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TinyEdit {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = new String[100];
+        System.out.println("Գրել տեքստ");
+        System.out.println("Գրել stop դուրս գալու համար");
+        for (int i = 0; i < 100; i++) {
+            str[i] = br.readLine();
+            if (str[i].equals("stop")) {
+                break;
+            }
+            System.out.println(str[i]);
+        }
+    }
+}
