63e34395bd707741897b6f6bcfc5cd926d06e52f
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-07-16 00:56:09 +0400

    InstanceOf

---
+++ src/chapters/chapter13/accessModifiers/InstanceOf.java
@@ -0,0 +1,17 @@
+package chapters.chapter13.accessModifiers;
+
+public class InstanceOf {
+    int i, j;
+}
+
+class B {
+    int i, j;
+}
+
+class C extends InstanceOf {
+    int k;
+}
+
+class D extends InstanceOf {
+    int k;
+}
