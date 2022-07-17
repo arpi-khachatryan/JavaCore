dd0e735bc3915bbe75efd1ab47e688f4e94e26a6
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-07-18 01:17:11 +0400

    IntStack

---
+++ src/chapters/chapter9/IntStack.java
@@ -0,0 +1,24 @@
+package chapters.chapter9;
+
+public interface IntStack {
+    void push(int item);
+
+    int pop();
+
+   /* default int[] popNElements(int n) {
+        return getElements(n);
+    }
+
+    default int[] skipAndPopNElements(int skip, int n) {
+        getElements(skip);
+        return getElements(n);
+    }
+
+    private getElements(int n) {
+        int[] elements = new int[n];
+        for (int i = 0; i < n; i++) {
+            elements[i] = pop();
+            return elements;
+        }
+    }*/
+}
