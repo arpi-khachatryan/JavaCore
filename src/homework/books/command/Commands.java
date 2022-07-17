524180bc8f4f8bc8b84d18a69ebaebebbfc9c06f
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-07-18 01:21:39 +0400

    Commands

--- src/homework/books/command/Commands.java
+++ src/homework/books/command/Commands.java
@@ -26,7 +26,7 @@ public interface Commands {
         System.out.println("Please, input " + PRINT_ALL_BOOKS + " to print all the books");
         System.out.println("Please, input " + PRINT_BOOKS_BY_AUTHOR_NAME + " to show all the books by your favorite author");
         System.out.println("Please, input " + PRINT_BOOKS_BY_GENRE + " to show all the books of your favorite genre");
-        System.out.println("Please, input " + PRINT_BOOKS_BY_PRICE_RANGE + " to show all the books at a specified price");
+        System.out.println("Please, input " + PRINT_BOOKS_BY_PRICE_RANGE + " to show all the books at aa.txt specified price");
         System.out.println("Please, input " + PRINT_AUTHOR_BY_GENDER + " to show the author by gender");
         System.out.println("Please, input " + PRINT_AUTHOR_BY_EMAIL + " to show the author by email");
         System.out.println("Please, input " + PRINT_ALL_AUTHORS + " to show all the authors");
@@ -37,11 +37,11 @@ public interface Commands {
         System.out.println("Please, input " + PRINT_ALL_BOOKS + " to print all the books");
         System.out.println("Please, input " + PRINT_BOOKS_BY_AUTHOR_NAME + " to show all the books by your favorite author");
         System.out.println("Please, input " + PRINT_BOOKS_BY_GENRE + " to show all the books of your favorite genre");
-        System.out.println("Please, input " + PRINT_BOOKS_BY_PRICE_RANGE + " to show all the books at a specified price");
+        System.out.println("Please, input " + PRINT_BOOKS_BY_PRICE_RANGE + " to show all the books at aa.txt specified price");
         System.out.println("Please, input " + PRINT_AUTHOR_BY_GENDER + " to show the author by gender");
         System.out.println("Please, input " + PRINT_AUTHOR_BY_EMAIL + " to show the author by email");
         System.out.println("Please, input " + PRINT_ALL_AUTHORS + " to show all the authors");
-        System.out.println("Please, input " + ADD_BOOK + " to add a book");
+        System.out.println("Please, input " + ADD_BOOK + " to add aa.txt book");
         System.out.println("Please, input " + ADD_AUTHOR + " to add the author");
     }
 }
