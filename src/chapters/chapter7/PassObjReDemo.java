42f980eb71d36336a99e6f3c3dd7ccc666c34504
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-07-18 20:40:27 +0400

    PassObjReDemo

--- src/chapters/chapter7/PassObjReDemo.java
+++ src/chapters/chapter7/PassObjReDemo.java
@@ -4,9 +4,9 @@ package chapters.chapter7;
 public class PassObjReDemo {
     public static void main(String[] args) {
         PassObjRe test = new PassObjRe(15, 20);
-        System.out.println("test.aa.txt & test.b: " + test.a + " " + test.b);
+        System.out.println("test.a & test.b: " + test.a + " " + test.b);
 
         test.meth(test);
-        System.out.println("test.aa.txt & test.b: " + test.a + " " + test.b);
+        System.out.println("test.a & test.b: " + test.a + " " + test.b);
     }
 }
