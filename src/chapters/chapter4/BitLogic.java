0f83a25b16df33b3ad57e2912aa76a4acf0f8976
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-07-18 20:45:46 +0400

    BitLogic

--- src/chapters/chapter4/BitLogic.java
+++ src/chapters/chapter4/BitLogic.java
@@ -14,13 +14,13 @@ public class BitLogic {
         int e = a ^ b;
         int f = (~a & b) | (a & ~b);
         int g = ~a & 0x0f;
-        System.out.println(" aa.txt = " + binary[a]);
+        System.out.println(" a = " + binary[a]);
         System.out.println(" b = " + binary[b]);
-        System.out.println(" aa.txt|b = " + binary[c]);
-        System.out.println(" aa.txt&b = " + binary[d]);
-        System.out.println(" aa.txt^b = " + binary[e]);
-        System.out.println("~aa.txt&b|aa.txt&~b = " + binary[f]);
-        System.out.println(" ~aa.txt = " + binary[g]);
+        System.out.println(" a|b = " + binary[c]);
+        System.out.println(" a&b = " + binary[d]);
+        System.out.println(" a^b = " + binary[e]);
+        System.out.println("~a&b|a&~b = " + binary[f]);
+        System.out.println(" ~a = " + binary[g]);
     }
 }
 
