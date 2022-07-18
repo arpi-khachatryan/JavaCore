13d1157b5083d97a825b111535d12464010f8e3f
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-07-18 20:40:39 +0400

    RetObDemo

--- src/chapters/chapter7/RetObDemo.java
+++ src/chapters/chapter7/RetObDemo.java
@@ -1,7 +1,5 @@
 package chapters.chapter7;
 
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
-
 public class RetObDemo {
     public static void main(String[] args) {
         RetOb ob1 = new RetOb(2);
@@ -9,10 +7,10 @@ public class RetObDemo {
 
         ob2 = ob1.incrByTen();
 
-        System.out.println("ob1.aa.txt: " + ob1.a);
-        System.out.println("ob2.aa.txt: " + ob2.a);
+        System.out.println("ob1.a: " + ob1.a);
+        System.out.println("ob2.a: " + ob2.a);
 
         ob2 = ob2.incrByTen();
-        System.out.println("ob2.aa.txt: " + ob2.a);
+        System.out.println("ob2.a: " + ob2.a);
     }
 }
