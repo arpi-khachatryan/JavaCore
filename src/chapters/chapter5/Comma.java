daaf6cc9cc4778829931c63f8dc377bc310a48a7
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-07-18 20:42:29 +0400

    Comma

--- src/chapters/chapter5/Comma.java
+++ src/chapters/chapter5/Comma.java
@@ -1,12 +1,10 @@
 package chapters.chapter5;
 
-import javax.swing.*;
-
 public class Comma {
     public static void main(String[] args) {
         int a, b;
         for (a = 1, b = 4; a < b; a++, b--) {
-            System.out.println("aa.txt = " + a);
+            System.out.println("a = " + a);
             System.out.println("b = " + b);
         }
     }
