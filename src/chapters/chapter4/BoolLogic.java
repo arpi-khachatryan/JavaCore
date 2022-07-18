376ae9280cff456dfcf9d681dc0f60c183374635
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-07-18 20:45:59 +0400

    BoolLogic

--- src/chapters/chapter4/BoolLogic.java
+++ src/chapters/chapter4/BoolLogic.java
@@ -9,12 +9,12 @@ public class BoolLogic {
         boolean e = a ^ b;
         boolean f = (!a & b) | (a & !b);
         boolean g = !a;
-        System.out.println("aa.txt = " + a);
+        System.out.println("a = " + a);
         System.out.println("b = " + b);
-        System.out.println("aa.txt | b = " + c);
-        System.out.println("aa.txt & b = " + d);
-        System.out.println("aa.txt ^ b = " + e);
-        System.out.println("(!aa.txt&b) | (aa.txt&!b) = " + f);
-        System.out.println("!aa.txt = " + g);
+        System.out.println("a | b = " + c);
+        System.out.println("a & b = " + d);
+        System.out.println("a ^ b = " + e);
+        System.out.println("(!a&b) | (a&!b) = " + f);
+        System.out.println("!a = " + g);
     }
 }
