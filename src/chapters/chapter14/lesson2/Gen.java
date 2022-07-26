078a5170ac3926b24cb3cc7ea4ca0f18acc7fbdd
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-07-26 16:40:51 +0400

    Gen

---
+++ src/chapters/chapter14/lesson2/Gen.java
@@ -0,0 +1,26 @@
+package chapters.chapter14.lesson2;
+
+public class Gen<T> {
+    T ob;
+
+    Gen(T o) {
+        ob = o;
+    }
+
+    T getob() {
+        return ob;
+    }
+}
+
+class RawDemo {
+    public static void main(String[] args) {
+        Gen<Integer> iOb = new Gen<>(88);
+        Gen<String> strOb = new Gen<>("oo");
+        System.out.println(iOb.getob());
+        System.out.println(strOb.getob());
+
+        Gen raw = new Gen(99.88);
+        double d = (Double) raw.getob();
+        System.out.println(d);
+    }
+}
