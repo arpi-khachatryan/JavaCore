99d6872a6e855618c9b8ba1c6e135f7e9bc32b9c
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-07-28 23:14:42 +0400

    FileUtil

--- src/homework/fileUtil/FileUtil.java
+++ src/homework/fileUtil/FileUtil.java
@@ -7,31 +7,35 @@ public class FileUtil {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        recursiveFileSearch();
+        //fileSearch();
         //contentSearch();
         //findLines();
         //printSizeOfPackage();
         //createFileWithContent();
     }
 
-    static void recursiveFileSearch() {
+    static void fileSearch() {
         String path = scanner.nextLine();
         String fileName = scanner.nextLine();
-        fileSearch(path, fileName);
+        recursiveFileSearch(path, fileName);
     }
 
-    static void fileSearch(String path, String fileName) {
+    static void recursiveFileSearch(String path, String fileName) {
         File myFolder = new File(path);
         if (myFolder.isDirectory()) {
             File[] files = myFolder.listFiles();
             for (File file : files) {
-                if (file.getName().equals(fileName)) {
-                    System.out.println(myFolder.getName() + " " + true);
-                    break;
-                } else {
-                    if (file.isDirectory()) {
-                        fileSearch(file.getPath(), fileName);
+                if (file.isFile()) {
+                    if (file.getName().equals(fileName)) {
+                        System.out.println(myFolder.getName() + " " + true);
+                        break;
+                    } else {
+                        if (file.isDirectory()) {
+                            recursiveFileSearch(file.getPath(), fileName);
+                        }
                     }
+                } else {
+                    System.out.println("File not found");
                 }
             }
         } else {
@@ -39,18 +43,58 @@ public class FileUtil {
         }
     }
 
+    //or-second option
+    static void fileSearch2() {
+        String path = scanner.nextLine();
+        String fileName = scanner.nextLine();
+        File file = new File(path, fileName);
+        System.out.println(file.exists());
+    }
+
     static void contentSearch() throws IOException {
         String path = scanner.nextLine();
-        File folder = new File(path);
         String keyword = scanner.nextLine();
-        File[] files = folder.listFiles();
-        for (File file : files) {
-            if (file.getName().endsWith(".txt")) {
-                try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file.getPath()))) {
+        File folder = new File(path);
+        if (folder.exists() && folder.isDirectory()) {
+            File[] files = folder.listFiles();
+            for (File file : files) {
+                if (file.getName().endsWith(".txt")) {
+                    try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file.getPath()))) {
+                        String line = "";
+                        while ((line = bufferedReader.readLine()) != null) {
+                            if (line.contains(keyword)) {
+                                System.out.println(file.getName());
+                                break;
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } else {
+            System.out.println("Folder not found");
+        }
+    }
+
+    //or-second option
+    static void contentSearch2() {
+        String path = scanner.nextLine();
+        String keyword = scanner.nextLine();
+        File folder = new File(path);
+        if (folder.exists() && folder.isDirectory()) {
+            File[] files = folder.listFiles(new FileFilter() {
+                @Override
+                public boolean accept(File pathname) {
+                    return pathname.getName().endsWith(".txt");
+                }
+            });
+            for (File file : files) {
+                try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
                     String line = "";
                     while ((line = bufferedReader.readLine()) != null) {
                         if (line.contains(keyword)) {
-                            System.out.println(file.getName());
+                            System.out.println(file.getAbsolutePath());
                             break;
                         }
                     }
@@ -58,62 +102,111 @@ public class FileUtil {
                     e.printStackTrace();
                 }
             }
+        } else {
+            System.out.println("Folder not found");
+        }
+    }
+
+    //or-third option
+    static void contentSearch3() {
+        String path = scanner.nextLine();
+        String keyword = scanner.nextLine();
+        findFile(path, keyword);
+    }
+
+    private static void findFile(String path, String keyword) {
+        File folder = new File(path);
+        if (folder.exists() && folder.isDirectory()) {
+            File[] files = folder.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isDirectory()) {
+                        findFile(file.getAbsolutePath(), keyword);
+                    } else {
+                        if (file.getName().endsWith(".txt")) {
+                            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+                                String line = "";
+                                while ((line = bufferedReader.readLine()) != null) {
+                                    if (line.contains(keyword)) {
+                                        System.out.println(file.getAbsolutePath());
+                                        break;
+                                    }
+                                }
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            } else {
+                System.out.println("Folder not found");
+            }
+        } else {
+            System.out.println("Folder not found");
         }
     }
 
     static void findLines() throws IOException {
         String txtPath = scanner.nextLine();
-        File path = new File(txtPath);
         String keyword = scanner.nextLine();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
-            String line = "";
-            int size = 0;
-            while ((line = bufferedReader.readLine()) != null) {
-                ++size;
-                if (line.contains(keyword)) {
-                    System.out.println("Line " + size + ": " + line);
+        File file = new File(txtPath);
+        if (file.exists() && file.isFile() && file.getName().endsWith(".txt")) {
+            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+                String line = "";
+                int size = 0;
+                while ((line = bufferedReader.readLine()) != null) {
+                    ++size;
+                    if (line.contains(keyword)) {
+                        line = line.replaceAll(keyword, "\u001B[33m" + keyword + "\u001B[0m");
+                        System.out.println("Line " + size + ": " + line);
+                    }
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
     static void printSizeOfPackage() throws IOException {
         String path = scanner.nextLine();
         File folder = new File(path);
-        File[] files = folder.listFiles();
-        long fileSize = 0;
-        for (File file : files) {
-            fileSize += file.length();
+        if (folder.exists() && folder.isDirectory()) {
+            File[] files = folder.listFiles();
+            long fileSize = 0;
+            for (File file : files) {
+                fileSize += file.length();
+            }
+            System.out.println(fileSize / (1024 * 1024) + " mb");
         }
-        System.out.println(fileSize / 1024);
     }
 
     static void createFileWithContent() throws IOException {
         String path = scanner.nextLine();
-        File folder = new File(path);
         String fileName = scanner.nextLine();
         String content = scanner.nextLine();
-        String filePath = folder.getPath();
-        filePath += File.separator + fileName;
-        File file = new File(filePath);
-        if (!file.exists()) {
-            file.createNewFile();
-            System.out.println(file);
-        }
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
-            bufferedWriter.write(content);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
-            String line = " ";
-            while ((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
+        File folder = new File(path);
+        if (folder.exists()) {
+            fileName = fileName.endsWith(".txt") ? fileName : fileName + ".txt";
+            File file = new File(path, fileName);
+            if (!file.exists()) {
+                file.createNewFile();
+                System.out.println(file);
+                try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+                    bufferedWriter.write(content);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+                    String line = " ";
+                    while ((line = bufferedReader.readLine()) != null) {
+                        System.out.println(line);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("A file with that name already exists");
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
