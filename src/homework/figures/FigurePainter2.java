6d85cb12f7ed746b3c1c7dcef9318a8e1b6e9f61
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-08-08 22:04:27 +0400

    FigurePainter2

--- src/homework/figures/FigurePainter2.java
+++ src/homework/figures/FigurePainter2.java
@@ -3,7 +3,7 @@ package homework.figures;
 public class FigurePainter2 {
     public static void main(String[] args) {
         for (int i = 0; i < 4; i++) {
-            for (int j = 4; j > i; j--) {
+            for (int j = 3; j > i; j--) {
                 System.out.print(" ");
             }
             for (int k = 0; k < i + 1; k++) {
@@ -12,7 +12,7 @@ public class FigurePainter2 {
             System.out.println();
         }
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < i + 2; j++) {
+            for (int j = 0; j < i + 1; j++) {
                 System.out.print(" ");
             }
             for (int k = 3; k > i; k--) {
