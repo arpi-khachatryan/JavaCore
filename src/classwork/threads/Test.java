52fa95e0c23f3bad34dcacdc6e8c215a937e7310
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-08-09 19:59:26 +0400

    Test

---
+++ src/classwork/threads/Test.java
@@ -0,0 +1,34 @@
+package classwork.threads;
+
+import java.util.Scanner;
+
+public class Test extends Thread {
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        myThread.start();
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        myThread.shutdown();
+    }
+}
+
+class MyThread extends Thread {
+    private volatile boolean running = true;
+
+    @Override
+    public void run() {
+        while (running) {
+            System.out.println("Hello");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void shutdown() {
+        this.running = false;
+    }
+}
