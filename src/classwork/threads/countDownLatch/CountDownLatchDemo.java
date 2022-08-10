ec595a9cb73ae3ee5473cb69e01b922265583939
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-08-10 16:38:58 +0400

    CountDownLatchDemo

---
+++ src/classwork/threads/countDownLatch/CountDownLatchDemo.java
@@ -0,0 +1,35 @@
+package classwork.threads.countDownLatch;
+
+import java.util.concurrent.CountDownLatch;
+
+public class CountDownLatchDemo {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(3);
+        new Work(countDownLatch);
+        new Work(countDownLatch);
+        new Work(countDownLatch);
+
+        countDownLatch.await();
+        System.out.println("all job done");
+    }
+}
+
+class Work extends Thread {
+    CountDownLatch countDownLatch;
+
+    public Work(CountDownLatch countDownLatch) {
+        this.countDownLatch = countDownLatch;
+        start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("done work");
+        countDownLatch.countDown();
+    }
+}
