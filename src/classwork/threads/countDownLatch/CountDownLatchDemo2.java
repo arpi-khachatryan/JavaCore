09ed359f930b691794bc950d79640ce24baed8a9
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-08-10 17:44:06 +0400

    CountDownLatchDemo2

---
+++ src/classwork/threads/countDownLatch/CountDownLatchDemo2.java
@@ -0,0 +1,43 @@
+package classwork.threads.countDownLatch;
+
+import java.util.concurrent.CountDownLatch;
+
+public class CountDownLatchDemo2 {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(10);
+
+        for (int i = 0; i < 10; i++) {
+            new DemoThread(latch).start();
+        }
+    }
+
+    private static class DemoThread extends Thread {
+        private final CountDownLatch latch;
+
+        private DemoThread(CountDownLatch latch) {
+            this.latch = latch;
+        }
+
+        @Override
+        public void run() {
+            try {
+                runUnsafe();
+            } catch (InterruptedException e) {
+                System.out.println(getName() + " interrupted");
+            }
+        }
+
+        private void runUnsafe() throws InterruptedException {
+            Thread.sleep((long) (Math.random() * 10000L));
+
+            System.out.println(getName() + " finished initialization");
+
+            latch.countDown();
+            latch.await();
+
+            System.out.println(getName() + " entered main phase");
+
+            Thread.sleep((long) (Math.random() * 10000));
+        }
+    }
+}
