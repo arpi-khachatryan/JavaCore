89e65ac07b3300e48bcbbcf56f101f02a5add6e5
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-08-10 14:20:49 +0400

    MainAtomic

---
+++ src/classwork/atomic/MainAtomic.java
@@ -0,0 +1,22 @@
+package classwork.atomic;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MainAtomic {
+    static AtomicInteger atomicInteger = new AtomicInteger(0);  // int i;
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 100; i++) {
+            new MyThread().start();
+        }
+        Thread.sleep(1000);
+        System.out.println(atomicInteger.get());   // System.out.println(i);
+    }
+
+    static class MyThread extends Thread {
+        @Override
+        public void run() {
+            atomicInteger.incrementAndGet(); //   i++;
+        }
+    }
+}
