335cad986c48d81a3fe4de085c554481c0834abb
arpikhachatryan <arpikhachatryan1@gmail.com>
2023-03-05 22:17:45 +0400

    Selection Sort

--- src/classwork/differentSorts/SelectionSort.java
+++ src/classwork/differentSorts/SelectionSort.java
@@ -1,7 +1,7 @@
 package classwork.differentSorts;
 
 public class SelectionSort {
-    static void selectionSort(int arr[]) {
+    static void selectionSort(int[] arr) {
         int count = 0;
         int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
