56314bc44154acb588a25a2565ea2430f3317683
arpikhachatryan <arpikhachatryan1@gmail.com>
2023-07-17 21:17:05 +0400

    Adapter

---
+++ src/classwork/patterns/adapter/Demo.java
@@ -0,0 +1,27 @@
+package classwork.patterns.adapter;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        RoundHole hole = new RoundHole(5);
+        RoundPeg peg = new RoundPeg(5);
+        if (hole.fits(peg)) {
+            System.out.println("Round peg r5 fits round hole r5.");
+        }
+
+        SquarePeg smallSqPeg = new SquarePeg(2);
+        SquarePeg largeSqPeg = new SquarePeg(20);
+        //hole.fits(smallSqPeg);
+
+
+        SquarePegAdapter smallSqPegAdapter = new SquarePegAdapter(smallSqPeg);
+        SquarePegAdapter largeSqPegAdapter = new SquarePegAdapter(largeSqPeg);
+        if (hole.fits(smallSqPegAdapter)) {
+            System.out.println("Square peg w2 fits round hole r5.");
+        }
+        if (!hole.fits(largeSqPegAdapter)) {
+            System.out.println("Square peg w20 does not fit into round hole r5.");
+        }
+    }
+}
